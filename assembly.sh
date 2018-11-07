6621a7d1d2537cecaca785854f9e00b9b0d74307
v.loskutov <v.loskutov@reply.de>
2018-11-07 12:09:42 +0100

    MAND-214: remove installation of gradle

--- assembly.sh
+++ assembly.sh
@@ -1,28 +1,12 @@
 #!/usr/bin/env bash
 RELEASE_VERSION=v4.0.0
 
-function installGradle()
-{
-    if ! [ -x "$(command -v gradle)" ]; then
-        if ! [ -x "$(command -v /opt/gradle/gradle-4.10.2/bin/gradle)" ]; then
-            wget https://services.gradle.org/distributions/gradle-3.4.1-bin.zip && \
-            mkdir /opt/gradle && \
-            unzip -d /opt/gradle gradle-4.10.2-bin.zip && \
-            echo "/opt/gradle/gradle-4.10.2/bin/gradle"
-        else
-            echo "/opt/gradle/gradle-4.10.2/bin/gradle"
-        fi
-    else
-        echo "gradle"
-    fi
-}
-
 GRADLE_P=$(installGradle) && \
 mkdir build_temp && \
 cd build_temp && \
 git clone https://github.com/apache/kafka/ && \
 cd kafka && \
-eval "$GRADLE_P installAll" && \
+gradle installAll && \
 cd .. && \
 echo "kafka is installed" && \
 git clone https://github.com/confluentinc/common && \
