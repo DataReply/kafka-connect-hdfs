75cce1d1a1676090f4573a2460713977b7fb4627
v.loskutov <v.loskutov@reply.de>
2018-11-08 12:30:03 +0100

    MAND-214: add skipTests options

--- assembly.sh
+++ assembly.sh
@@ -29,39 +29,4 @@ cd .. && \
 echo "kafka-connect-storage-common is installed" && \
 cd .. && \
 rm -r build_temp && \
-mvn clean package
-
-#mkdir build_temp && \
-#cd build_temp && \
-#git clone https://github.com/apache/kafka/ && \
-#cd kafka && \
-#gradle installAll && \
-#cd .. && \
-#echo "kafka is installed" && \
-#git clone https://github.com/confluentinc/common && \
-#cd common && \
-#git checkout tags/v4.0.0 -b 4.0.0-datareply && \
-#mvn clean install && \
-#cd .. && \
-#echo "common is installed" && \
-#git clone https://github.com/confluentinc/rest-utils && \
-#cd rest-utils && \
-#git checkout tags/v4.0.0 -b 4.0.0-datareply && \
-#mvn clean install && \
-#cd .. && \
-#echo "rest-utils is installed" && \
-#git clone https://github.com/confluentinc/schema-registry && \
-#cd schema-registry && \
-#git checkout tags/v4.0.0 -b 4.0.0-datareply && \
-#mvn clean install && \
-#cd .. && \
-#echo "schema-registry is installed" && \
-#git clone https://github.com/confluentinc/kafka-connect-storage-common && \
-#cd kafka-connect-storage-common && \
-#git checkout tags/v4.0.0 -b 4.0.0-datareply && \
-#mvn clean install -DskipTests=true && \
-#cd .. && \
-#echo "kafka-connect-storage-common is installed" && \
-#cd .. && \
-#rm -r build_temp && \
-#mvn clean package
+mvn clean package -DskipTests=true
