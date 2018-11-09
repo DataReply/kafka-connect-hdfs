0e5d7570df3bf4e3ca5aa7b16ae23cfd5b62037e
v.loskutov <v.loskutov@reply.de>
2018-11-09 15:13:36 +0100

    MAND-214: skip all the tests

--- assembly.sh
+++ assembly.sh
@@ -4,22 +4,22 @@ RELEASE_VERSION=v4.0.0
 cd build_temp && \
 unzip kafka.zip && \
 cd kafka-trunk && \
-gradle -Dhttps.proxyHost=$1 -Dhttps.proxyPort=$2 installAll && \
+gradle -Dhttps.proxyHost=$1 -Dhttps.proxyPort=$2 installAll -x test && \
 cd .. && \
 echo "kafka is installed" && \
 unzip common.zip && \
 cd common-4.0.0 && \
-mvn clean install && \
+mvn clean install -DskipTests=true && \
 cd .. && \
 echo "common is installed" && \
 unzip rest-utils.zip && \
 cd rest-utils-4.0.0 && \
-mvn clean install && \
+mvn clean install -DskipTests=true && \
 cd .. && \
 echo "rest-utils is installed" && \
 unzip schema-registry.zip && \
 cd schema-registry-4.0.0 && \
-mvn clean install && \
+mvn clean install -DskipTests=true && \
 cd .. && \
 echo "schema-registry is installed" && \
 unzip kafka-connect-storage-common.zip && \
