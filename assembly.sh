b1a5f1e1c52807358b8b198ed3c3e6129a1f51c5
v.loskutov <v.loskutov@reply.de>
2018-11-13 12:31:31 +0100

    MAND-214: make every step in bash quiet to avoid overwhelming logs

--- assembly.sh
+++ assembly.sh
@@ -7,31 +7,31 @@ wget -q -O common.zip https://github.com/confluentinc/common/archive/v4.0.0.zip 
 wget -q -O rest-utils.zip https://github.com/confluentinc/rest-utils/archive/v4.0.0.zip && \
 wget -q -O schema-registry.zip https://github.com/confluentinc/schema-registry/archive/v4.0.0.zip && \
 wget -q -O kafka-connect-storage-common.zip https://github.com/confluentinc/kafka-connect-storage-common/archive/v4.0.0.zip && \
-unzip kafka.zip && \
+unzip -q kafka.zip && \
 cd kafka-trunk && \
-gradle -Dhttps.proxyHost=$1 -Dhttps.proxyPort=$2 installAll -x test && \
+gradle -Dhttps.proxyHost=$1 -Dhttps.proxyPort=$2 installAll -q -x test && \
 cd .. && \
 echo "kafka is installed" && \
-unzip common.zip && \
+unzip -q common.zip && \
 cd common-4.0.0 && \
-mvn clean install -DskipTests=true && \
+mvn clean install -q -DskipTests=true && \
 cd .. && \
 echo "common is installed" && \
-unzip rest-utils.zip && \
+unzip -q rest-utils.zip && \
 cd rest-utils-4.0.0 && \
-mvn clean install -DskipTests=true && \
+mvn clean install -q -DskipTests=true && \
 cd .. && \
 echo "rest-utils is installed" && \
-unzip schema-registry.zip && \
+unzip -q schema-registry.zip && \
 cd schema-registry-4.0.0 && \
-mvn clean install -DskipTests=true && \
+mvn clean install -q -DskipTests=true && \
 cd .. && \
 echo "schema-registry is installed" && \
-unzip kafka-connect-storage-common.zip && \
+unzip -q kafka-connect-storage-common.zip && \
 cd kafka-connect-storage-common-4.0.0 && \
-mvn clean install -DskipTests=true && \
+mvn clean install -q -DskipTests=true && \
 cd .. && \
 echo "kafka-connect-storage-common is installed" && \
 cd .. && \
 rm -r -f build_temp && \
-mvn clean package -DskipTests=true
+mvn clean package -q -DskipTests=true
