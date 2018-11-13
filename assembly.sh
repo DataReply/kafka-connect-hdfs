3ec3f9f0c2143b001638f3a4070f77d7fd746174
v.loskutov <v.loskutov@reply.de>
2018-11-13 11:46:38 +0100

    MAND-214: change name of downloaded archives

--- assembly.sh
+++ assembly.sh
@@ -2,11 +2,11 @@
 RELEASE_VERSION=v4.0.0
 
 cd build_temp && \
-wget https://github.com/apache/kafka/archive/trunk.zip && \
-wget https://github.com/confluentinc/common/archive/v4.0.0.zip && \
-wget https://github.com/confluentinc/rest-utils/archive/v4.0.0.zip && \
-wget https://github.com/confluentinc/schema-registry/archive/v4.0.0.zip && \
-wget https://github.com/confluentinc/kafka-connect-storage-common/archive/v4.0.0.zip && \
+wget -O kafka.zip https://github.com/apache/kafka/archive/trunk.zip && \
+wget -O common.zip https://github.com/confluentinc/common/archive/v4.0.0.zip && \
+wget -O rest-utils.zip https://github.com/confluentinc/rest-utils/archive/v4.0.0.zip && \
+wget -O schema-registry.zip https://github.com/confluentinc/schema-registry/archive/v4.0.0.zip && \
+wget -O kafka-connect-storage-common.zip https://github.com/confluentinc/kafka-connect-storage-common/archive/v4.0.0.zip && \
 unzip kafka.zip && \
 cd kafka-trunk && \
 gradle -Dhttps.proxyHost=$1 -Dhttps.proxyPort=$2 installAll -x test && \
